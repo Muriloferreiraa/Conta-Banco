e2ad6a6efe5b644ca8deed627e43d7f042ae3f79
MuriloFerreiraa <muriloferrei.ra@outlook.com>
2024-05-24 12:11:58 -0300

    #atualização

---
+++ src/ContaTerminalScanner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminalScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        
+        System.out.println("Digite o numero da conta: ");
+        float numeroConta = scanner.nextFloat();
+        scanner.nextLine();
+        
+        System.out.println("Digite o número da agência: ");
+        float numeroAgencia = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Número da conta: " + numeroConta);
+        System.out.println("Número da agência: " + numeroAgencia);
+
+        scanner.close();
+    }
+}
